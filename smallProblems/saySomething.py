47f9408d59609705dc48873488ddc3a3233f5c6b
Pavly Farag <e.bavly.farag@gmail.com>
2021-01-19 14:45:23 +0100

    update solution to have user inputs in a list instead of concatenated string

--- smallProblems/saySomething.py
+++ smallProblems/saySomething.py
@@ -1,13 +1,15 @@
-Sentence = ""
 def is_question(sentence):
     questions = ("how","what","who","why","which","when","where","whom","is","are","have","did","do")
     if sentence.lower().startswith(questions):
         return sentence.capitalize() + "?"
     else:
         return sentence.capitalize() + "."
+sentence_list = []
 while True:
     user_input = input("Say somthing: ")
     if user_input == "\\end":
         break
-    Sentence = Sentence + " " + is_question(user_input)
-print(Sentence)
+    else:
+        sentence_list.append(is_question(user_input))
+
+print(" ".join(sentence_list))
